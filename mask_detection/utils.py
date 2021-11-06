8d00857cb858a9569ad42b7bb6dfed352f552b36
Gireg Roussel <giregroussel@free.fr>
2021-11-06 20:31:47 +0100

    [UPDATE] train_mask: update real time and image files

---
+++ mask_detection/utils.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+# Principal packages
+import os
+import torch
+from torchvision import transforms
+
+import model
+
+# Helper libraries
+import argparse
+import cv2
+
+import numpy as np
+
+output_path = 'mask_detector/'
+
+def predict(image, model):
+    # define preprocess transforms
+		transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(224),
+    transforms.ToTensor()
+    ])
+
+    # convert to RGB format
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		image = transform(image)
+    # add batch dimension
+		image = torch.unsqueeze(image, 0)
+		with torch.no_grad():
+			preds = model(image)
+		return preds
+  
+def load_models(device, faceModelPath):
+	# load our serialized face detector model from disk
+	print("[INFO] loading face detector model...")
+	prototxtPath = os.path.sep.join([faceModelPath, "deploy.prototxt"])
+	weightsPath = os.path.sep.join([faceModelPath,
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+	faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+	# load the face mask detector model from disk
+	print("[INFO] loading face mask detector model...")
+	# initialize the model and load the trained weights
+	maskModel = model.FaceMaskDetectorModel().to(device)
+	# model.load_model(output_path+'model.pth')
+	checkpoint = torch.load(output_path+'model.pth', map_location=device)
+	maskModel.load_state_dict(checkpoint['model_state_dict'])
+	maskModel.eval()
+
+	return maskModel, faceNet
+
+
+def display_result(locations, predictions, frame):
+	# loop over the detected face locations and their corresponding locations
+	for (box, pred) in zip(locations, predictions):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "No Mask" if pred else "Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+			
+		# include the probability in the label
+		# label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+		# display the label and bounding box rectangle on the output frame
+		cv2.putText(frame, label, (startX, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+def detect_and_predict_mask(frame, faceNet, maskModel, default_confidence):
+	# grab the dimensions of the frame and then construct a blob
+	# from it
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+		(104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the face detections
+	print("[INFO] Computing face detections...")
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	# initialize our list of faces, their corresponding locations,
+	# and the list of predictions from our face mask network
+	faces = []
+	locs = []
+	preds = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the detection
+		detection_confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is
+		# greater than the minimum confidence
+		if detection_confidence > default_confidence:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# ensure the bounding boxes fall within the dimensions of
+			# the frame
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			# ordering, resize it to 224x224, and preprocess it
+			face = frame[startY:endY, startX:endX]
+
+			# pass the face through the model to determine if the face
+			# has a mask or not
+			predictions = predict(face, maskModel)
+			_, pred = torch.max(predictions.data, 1)
+
+				# add the face and bounding boxes to their respective
+				# lists
+			if (pred):
+				faces.append(face)
+				preds.append(pred)
+				locs.append((startX, startY, endX, endY))
+
+	# return a 2-tuple of the face locations and their corresponding
+	# locations
+	return (faces, locs, preds)
