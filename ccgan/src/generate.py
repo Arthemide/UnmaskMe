24c12cd76e8adb875387f0f73f80445628e34bbe
Elkokonut <35450572+Elkokonut@users.noreply.github.com>
2021-11-22 11:38:18 +0100

    [FIX] Fix ccgan/src/generate.py format

--- ccgan/src/generate.py
+++ ccgan/src/generate.py
@@ -1,33 +1,30 @@
 import os
 from PIL import Image
 
-import numpy as np
-
 import torch
 
-from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from torch.autograd import Variable
 import torchvision.transforms.functional as F
-from torchvision.utils import save_image
-
-from models import Generator
 from datasets import UniqueDataset
+from models import Generator
+from PIL import Image
+from torch.autograd import Variable
 
 
 def load_generator(filename, eval=True, device=None):
-    if (os.path.isfile(filename)):
+    if os.path.isfile(filename):
         print("Loading model %s" % filename)
         if device is None:
-            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            device = torch.device(
+                "cuda:0" if torch.cuda.is_available() else "cpu")
         checkpoint = torch.load(filename, map_location=torch.device(device))
 
         generator = Generator((3, 128, 128))
 
-        generator.load_state_dict(checkpoint['generator_state_dict'])
+        generator.load_state_dict(checkpoint["generator_state_dict"])
         generator = generator.to(device)
 
-        if eval == True:
+        if eval is True:
             generator.eval()
         else:
             generator.train()
@@ -49,21 +46,27 @@ transforms_lr = [
 ]
 
 
-def generate_face(generator, mask, masked, transforms_x=transforms_, transforms_lr=transforms_lr):
+def generate_face(
+    generator, mask, masked, transforms_x=transforms_, transforms_lr=transforms_lr
+):
     generator.eval()
     white = Image.new("L", masked.size, 255)
     mask_applied = Image.composite(white, masked, mask)
 
-    loader = DataLoader(UniqueDataset(image=mask_applied, transforms_x=transforms_, transforms_lr=transforms_lr),
-                        batch_size=1)
+    loader = DataLoader(
+        UniqueDataset(image=mask_applied, transforms_x=transforms_x,
+                      transforms_lr=transforms_lr),
+        batch_size=1)
     for i, b in enumerate(loader):
         with torch.no_grad():
-            img = generator(Variable(b['x']), Variable(b['x_lr']))
+            img = generator(Variable(b["x"]), Variable(b["x_lr"]))
 
-    pil_image = transforms.Compose({
-        transforms.Resize((masked.size[1], masked.size[0])), transforms.Normalize(
-            (-0.5, -0.5, -0.5), (2, 2, 2))
-    })(img[0])
+    pil_image = transforms.Compose(
+        {
+            transforms.Resize((masked.size[1], masked.size[0])),
+            transforms.Normalize((-0.5, -0.5, -0.5), (2, 2, 2))
+        }
+    )(img[0])
 
     pil_image = transforms.ToPILImage()(pil_image)
 
