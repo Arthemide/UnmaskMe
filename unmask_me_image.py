16a3a1977c69c953fdce90c530b54d194b4d3d73
Gireg Roussel <giregroussel@free.fr>
2022-01-06 16:07:36 +0100

    [UDPATE]: update image file yolo links

--- unmask_me_image.py
+++ unmask_me_image.py
@@ -8,12 +8,13 @@ from mask_detection import utils as mask_utils
 from mask_segmentation import utils as segmentation_utils
 from ccgan import generate as gan_utils
 from ressources import (
-    get_YOLOv5,
     replace_face,
     get_face_detector_model,
     get_mask_detector_model,
     get_mask_segmentation_model,
     get_ccgan_model,
+    get_YOLOv5_repo,
+    get_YOLOv5_model
 )
 
 try:
@@ -21,7 +22,8 @@ try:
     get_mask_detector_model()
     get_mask_segmentation_model()
     get_ccgan_model()
-    get_YOLOv5()
+    get_YOLOv5_repo()
+    get_YOLOv5_model()
 except:
 
     print("error")
@@ -42,28 +44,11 @@ if __name__ == "__main__":
         "-c",
         "--confidence",
         type=float,
-        default=0.5,
+        default=0.75,
         help="minimum probability to filter weak detections",
-    )
-    ap.add_argument(
-        "-d",
-        "--data",
-        type=str,
-        default='./mask_detection/YOLOv5/data/mask_data.yaml',
-        help="yaml dataset file path",
-    )
-    ap.add_argument(
-        "-w",
-        "--weights",
-        type=str,
-        default='./model_weights/mask_face_detector.pt',
-        help="model weights path",
     )
     args = vars(ap.parse_args())
 
-    maskModel, faceNet = mask_utils.load_models(
-        device, "model_weights/face_detector", "model_weights/mask_detector_model.pth"
-    )
     segmentation_model = segmentation_utils.load_model(
         device, "model_weights/model_mask_segmentation.pth"
     )
@@ -72,10 +57,11 @@ if __name__ == "__main__":
 
     image = cv2.imread(args["image"])
     if image is not None:
-        (faces, locs) = run_model(weights=args["weights"], data=args["data"], conf_thres=args["confidence"], iou_thres=0.45, max_det=1000, source=args["image"])
-        frame = mask_utils.display_result(locs, frame=image)
-        cv2.imshow("Output", frame)
-        cv2.waitKey(0)
+        (faces, locs) = run_model(
+            weights="./model_weights/mask_face_detector.pt",
+            data="./mask_detection/YOLOv5/data/mask_data.yaml",
+            conf_thres=args["confidence"],
+            source=args["image"])
 
         if len(faces) != 0:
             # segment the mask on faces
@@ -90,8 +76,8 @@ if __name__ == "__main__":
 
             # show the output image
             print("[INFO] Job done, showing image")
-            # cv2.imshow("Output", image)
-            # cv2.waitKey(0)
+            cv2.imshow("Output", image)
+            cv2.waitKey(0)
     else:
         print("[INFO] Bad image path")
 
